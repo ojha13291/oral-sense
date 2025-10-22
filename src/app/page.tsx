5be80efc1dbd8bf543c22deece90283f1e85eec1
Ayush <kiliniko.ai.official@gmail.com>
2025-10-22 22:46:20 +0530

    added landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,27 @@
-import { SignedOut, SignUpButton, SignedIn, SignOutButton } from '@clerk/nextjs';
-import { Button } from '@/components/ui/button';
+import CTA from "@/components/landing/CTA";
+import Footer from "@/components/landing/Footer";
+import Header from "@/components/landing/Header";
+import Hero from "@/components/landing/Hero";
+import HowItWorks from "@/components/landing/HowItWorks";
+import PricingSection from "@/components/landing/PricingSection";
+import WhatToAsk from "@/components/landing/WhatToAsk";
+import { syncUser } from "@/lib/actions/users";
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
 
 export default function Home() {
   return (
     <div>
-      <h1>Home Page</h1>
-      <SignedOut>
-        <SignUpButton mode='modal'>Sign Up
-        </SignUpButton>
-      </SignedOut>
-
-      <SignedIn>
-        <SignOutButton>
-          <Button variant='destructive'>Log Out</Button>
-        </SignOutButton>
-      </SignedIn>
+      <div className="min-h-screen bg-background">
+        <Header />
+        <Hero />
+        <HowItWorks />
+        <WhatToAsk />
+        <PricingSection />
+        <CTA />
+        <Footer />
+      </div>
     </div>
   );
 }
